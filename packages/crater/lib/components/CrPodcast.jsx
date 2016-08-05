3fbcbe1fbc646b4223691b083dfa1278aedfff78
Sacha Greif <sacha357@gmail.com>
2016-08-05 10:06:58 +0900

    minor css

--- packages/crater/lib/components/CrPodcast.jsx
+++ packages/crater/lib/components/CrPodcast.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { DocumentContainer } from "meteor/utilities:react-list-container";
 import { FormattedMessage, FormattedRelative } from 'react-intl';
+import Posts from 'meteor/nova:posts';
 
-// note: post.postedAt is sometimes empty when SSR'ing (see https://github.com/kadirahq/flow-router/issues/639)
+// note: post.postedAt is sometimes the empty object when SSR'ing (see https://github.com/kadirahq/flow-router/issues/639)
 const Podcast = ({post}) => {
+  console.log("// post")
+  console.log(post.postedAt)
   return (
     <div className="podcast-content">
       <h3 className="podcast-title">{post.title}</h3>
-      <div className="podcast-date">{post.postedAt ? <FormattedRelative value={post.postedAt}/> : null}</div>
+      <div className="podcast-date">{post.postedAt && !_.isObject(post.postedAt) ? <FormattedRelative value={post.postedAt}/> : null}</div>
       <iframe frameBorder='0' height='36px' scrolling='no' seamless src={`https://simplecast.com/e/${post.podcastId}?style=light`} width='100%'></iframe>
       <div className="podcast-links">
-        <a className="podcast-discuss" href="#">Discuss this episode</a>
+        <a className="podcast-discuss" href={Posts.getPageUrl(post)}>Discuss this episode</a>
         |
         <a className="podcast-subscribe" href="#">Subscribe on iTunes</a>
       </div>
